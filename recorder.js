778c4241be744b82805066be5b8c3339320ba087
NewbieOrange <NewbieOrange@users.noreply.github.com>
2021-02-15 17:23:20 +0800

    Add dialog recording support

--- recorder.js
+++ recorder.js
@@ -1,17 +1,31 @@
-function record() {
-    instrument('android.app.Activity', 'dispatchTouchEvent', function (event) {
-        send('Touch event intercepted: ' + event)
-        return this.dispatchTouchEvent(event)
+function record_touch(typename) {
+    instrument(typename, 'dispatchTouchEvent', function (event) {
+        send('Touch event intercepted: ' + event);
+        return this.dispatchTouchEvent(event);
     });
-    instrument('android.app.Activity', 'dispatchKeyEvent', function (event) {
-        send('Key event intercepted: ' + event)
-        return this.dispatchKeyEvent(event)
+}
+
+function record_key(typename) {
+    instrument(typename, 'dispatchKeyEvent', function (event) {
+        send('Touch event intercepted: ' + event);
+        return this.dispatchKeyEvent(event);
     });
+}
+
+function record_location() {
     instrument('android.location.LocationManager', 'getLastKnownLocation', function (provider) {
         const location = this.getLastKnownLocation(provider)
         send('Location event intercepted: ' + location)
         return location
     });
+}
+
+function record() {
+    record_touch('android.app.Activity');
+    record_touch('android.app.Dialog');
+    record_key('android.app.Activity');
+    record_key('android.app.Dialog');
+    record_location();
 }
 
 rpc.exports = {
