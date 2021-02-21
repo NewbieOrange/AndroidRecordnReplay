c5460144d585c4d6b90ab992f6a03b31b4f079f3
NewbieOrange <NewbieOrange@users.noreply.github.com>
2021-02-22 00:21:28 +0800

    Minor refactor and improvement

--- recorder.py
+++ recorder.py
@@ -1,20 +1,42 @@
-import frida
-import script
 import sys
+
+import adbutils
+import frida
+
+import script
 
 
 class Recorder:
-    def __init__(self, session):
+    def __init__(self, session: frida.core.Session, device: adbutils.AdbDevice):
         self.session = session
+        self.device = device
 
     def record(self):
+        self.record_adb()
+        self.record_frida()
+
+    def record_adb(self):
+        self.device.shell('getevent -tt')
+
+    def record_frida(self):
         with open('recorder.js', 'r') as f:
             s = script.Script(session, f.read())
+        s.set_on_message(self.on_message)
         s.rpc.record()
+
+    def on_message(self, msg: dict, _):
+        if msg['type'] == 'send':
+            print(msg['payload'])
+        else:
+            print(msg)
+
+    def close(self):
+        print('closed')
+        self.session.detach()
 
 
 if __name__ == '__main__':
-    session = frida.get_usb_device().attach('com.google.android.apps.messaging')
+    session = frida.get_usb_device().attach('com.exatools.sensors')
     recorder = Recorder(session)
     recorder.record()
     sys.stdin.read()  # pause for logs
--- script.py
+++ script.py
@@ -7,10 +7,14 @@ with open('utils.js', 'r') as f:
 class Script:
     def __init__(self, session: frida.core.Session, source: str):
         self.session = session
+        self._on_message = None
         self.script = session.create_script(utils + source)
         self.script.on('message', self.on_message)
         self.script.load()
         self.rpc = self.script.exports
 
-    def on_message(self, message, data):
-        print(message)
+    def on_message(self, msg, data):
+        self._on_message(msg, data)
+
+    def set_on_message(self, callback):
+        self._on_message = callback
