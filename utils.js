7c55d361960b540547c4e1480a86c3df9fcbe3d5
NewbieOrange <NewbieOrange@users.noreply.github.com>
2021-02-22 00:20:49 +0800

    Add functions for overload and arrays

--- utils.js
+++ utils.js
@@ -1,6 +1,27 @@
+function instrumentOverload(typename, funcname, overload, impl) {
+    Java.perform(() => {
+        const type = Java.use(typename);
+        type[funcname].overload(...overload).implementation = impl;
+    })
+}
+
 function instrument(typename, funcname, impl) {
     Java.perform(() => {
         const type = Java.use(typename);
         type[funcname].implementation = impl;
     })
+}
+
+function replaceAt(string, index, replacement) {
+    return string.substr(0, index) + replacement + string.substr(index + replacement.length);
+}
+
+function javaArrayToString(array) {
+    let string = '['
+
+    for (let i = 0; i < array.length; i += 1) {
+        string += array[i] + ','
+    }
+
+    return replaceAt(string, string.length - 1, ']')
 }
