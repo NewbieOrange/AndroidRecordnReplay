90cd4c01414c7a51f8e1d49832a93c5c4afa091e
NewbieOrange <NewbieOrange@users.noreply.github.com>
2021-03-15 11:30:55 +0800

    Kill frida-server when running a new one

--- frida-server.bat
+++ frida-server.bat
@@ -2,4 +2,5 @@
 set PATH=%PATH%;D:\android-sdk\platform-tools\
 adb push "lib\frida-server" "/data/local/tmp/"
 adb shell "chmod 755 /data/local/tmp/frida-server"
+adb shell "ps | grep frida-server | awk '{print $2}' | xargs kill -9"
 adb shell "/data/local/tmp/frida-server &"
