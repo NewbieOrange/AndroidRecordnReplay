077ad735c5f0b0d903e4542a69ad1c0850da8f37
NewbieOrange <NewbieOrange@users.noreply.github.com>
2021-03-18 20:38:12 +0800

    Fix frida-server script not using root and not returning immediately

--- frida-server.bat
+++ frida-server.bat
@@ -1,6 +1,7 @@
 @echo off
 set PATH=%PATH%;D:\android-sdk\platform-tools\
+adb root
 adb push "lib\frida-server" "/data/local/tmp/"
 adb shell "chmod 755 /data/local/tmp/frida-server"
 adb shell "ps | grep frida-server | awk '{print $2}' | xargs kill -9"
-adb shell "/data/local/tmp/frida-server &"
+adb shell "nohup /data/local/tmp/frida-server 2>/dev/null 1>/dev/null &"
